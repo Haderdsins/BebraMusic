173eb88996b506000d4f6785e35d7ed320d58652
Максим <maxifly0202@gmail.com>
2023-11-01 20:41:48 +0300

    Сделали самое крутое оформление в мире, нововведений куча, вы не останетесь равнодушными

---
+++ BebraMusic.UI/Pages/Authors/Create.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using BebraMusic.UI.DataBase;
+using BebraMusic.UI.DataBase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BebraMusic.UI.Pages.Authors;
+
+public class Create : PageModel
+{
+    private readonly BebraMusicDbContext _dbContext;
+    [BindProperty]
+    public Author Author { get; set; } = new Author();
+
+    public Create(BebraMusicDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public IActionResult OnPost()
+    {
+        _dbContext.Add(Author);
+        _dbContext.SaveChanges();
+        return RedirectToPage("Index");
+    }
+    
+    
+}
