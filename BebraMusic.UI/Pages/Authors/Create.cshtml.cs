eff1ccd541a3e38fb3ed240740e7b879d01e7e4f
Максим <maxifly0202@gmail.com>
2023-11-10 14:33:17 +0300

    Добавили ошибку при вводе уже существующего id

--- BebraMusic.UI/Pages/Authors/Create.cshtml.cs
+++ BebraMusic.UI/Pages/Authors/Create.cshtml.cs
@@ -2,25 +2,37 @@
 using BebraMusic.UI.DataBase.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
-namespace BebraMusic.UI.Pages.Authors;
-
-public class Create : PageModel
+namespace BebraMusic.UI.Pages.Authors
 {
-    private readonly BebraMusicDbContext _dbContext;
-    [BindProperty]
-    public Author Author { get; set; } = new Author();
+    public class Create : PageModel
+    {
+        private readonly BebraMusicDbContext _dbContext;
+        
+        [BindProperty]
+        public Author Author { get; set; } = new Author();
 
-    public Create(BebraMusicDbContext dbContext)
-    {
-        _dbContext = dbContext;
+        public Create(BebraMusicDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult OnPost()
+        {
+            // Проверяем, существует ли артист с таким id
+            if (_dbContext.Authors.Any(a => a.Id == Author.Id))
+            {
+                TempData["ErrorMessage"] = "Артист с таким Id уже существует.";
+                return RedirectToPage();
+            }
+
+            // Если проверка прошла успешно, добавляем артиста в базу данных
+            _dbContext.Add(Author);
+            _dbContext.SaveChanges();
+
+            return RedirectToPage("Index");
+        }
     }
-    public IActionResult OnPost()
-    {
-        _dbContext.Add(Author);
-        _dbContext.SaveChanges();
-        return RedirectToPage("Index");
-    }
-    
-    
 }
