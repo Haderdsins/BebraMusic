c64852a94ddbe598055f63e53c9fca668fde90d8
Максим <maxifly0202@gmail.com>
2023-11-11 05:51:55 +0300

    Легендарная сборка

---
+++ BebraMusic.UI/Pages/Genres/Create.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using BebraMusic.UI.DataBase;
+using BebraMusic.UI.DataBase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BebraMusic.UI.Pages.Genres
+
+{
+    public class Create : PageModel
+    {
+        private readonly BebraMusicDbContext _dbContext;
+        
+        [BindProperty]
+        public Genre Genre { get; set; } = new Genre();
+
+        public Create(BebraMusicDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult OnPost()
+        {
+            {
+                _dbContext.Add(Genre);
+                _dbContext.SaveChanges();
+
+                Genre = new Genre();
+
+                return RedirectToPage("Index");
+            }
+
+            return Page();
+        }
+        
+    }
+}
---
+++ BebraMusic.UI/Pages/Genres/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using BebraMusic.UI.DataBase;
+using BebraMusic.UI.DataBase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BebraMusic.UI.Pages.Genres
+{
+    public class Index : PageModel
+    {
+        private readonly BebraMusicDbContext _dbContext;
+
+        public IEnumerable<Genre>? Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; } 
+
+        public Index(BebraMusicDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void OnGet()
+        {
+            var query = _dbContext.Genres.AsQueryable();
+
+            if (SearchString != null)
+            {
+                // Язык запросов LinQ - позволяет работать с коллекциями, Contains - метод позволяет сопоставлять строку со строкой в таблице, x - это объект класса
+                query = query.Where(x => x.Name.Contains(SearchString));
+            }
+
+            // Добавляем сортировку по Id
+            Genres = query.OrderBy(x => x.Id).ToList();
+        }
+    }
+}
